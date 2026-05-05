36bbfee5a7a89cc2041573889fc53841a3b9442a
danielrpg <daniel.fernandezgp@gmail.com>
2026-05-05 07:26:29 -0400

    como usar instance of para identificar el polimorfismo

--- src/platzi/play/util/FileUtils.java
+++ src/platzi/play/util/FileUtils.java
@@ -2,6 +2,7 @@ package platzi.play.util;
 
 import platzi.play.contenido.*;
 
+import javax.print.Doc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -25,9 +26,19 @@ public class FileUtils {
                 contenido.getIdioma().name(),
                 contenido.getCalidad().name()
         );
+
+        String lineaFinal;
+
+        if (contenido instanceof Documental documental) {
+            // Documental documental = (Documental) contenido;
+            lineaFinal = "DOCUMENTAL" + SEPARADOR + linea + SEPARADOR + documental.getNarrador();
+        } else {
+            lineaFinal = "PELICULA" + SEPARADOR + linea;
+        }
+
         try {
             Files.writeString(Paths.get(NOMBRE_ARCHIVO),  // le mandamos el nombre del archivo el path
-                        linea + System.lineSeparator(), // mandamos la linea mas un separador de linea
+                        lineaFinal + System.lineSeparator(), // mandamos la linea mas un separador de linea
                     StandardOpenOption.CREATE, // le decimos que cree el archivo si no existe
                     StandardOpenOption.APPEND // agregamos el archivo no lo sobreescribimos
             );
@@ -43,17 +54,28 @@ public class FileUtils {
             List<String> lineas = Files.readAllLines(Paths.get(NOMBRE_ARCHIVO));
 
             lineas.forEach(linea -> {
-                String[] datos = linea.split(SEPARADOR);
-                if (datos.length == 5) {
-                    String titulo = datos[0];
-                    int duracion = Integer.parseInt(datos[1]);
-                    Genero genero = Genero.valueOf(datos[2].toUpperCase());
-                    double calificacion = datos[3].isBlank() ? 0 : Double.parseDouble(datos[3]);
-                    LocalDate fechaEstreno = datos[4].isBlank() ? null : LocalDate.parse(datos[4]);
+                String[] datos = linea.split("\\" + SEPARADOR);
+                String tipoContenido = datos[0];
 
-                    Contenido contenido = new Contenido(titulo, duracion, genero, Idioma.ESPANOL, Calidad.ALTA, calificacion);
+                if (("PELICULA".equals(tipoContenido) && datos.length == 7) || ("DOCUMENTAL".equals(tipoContenido) && datos.length == 8)) {
+                    String tipo = datos[0];
+                    String titulo = datos[1];
+                    int duracion = Integer.parseInt(datos[2]);
+                    Genero genero = Genero.valueOf(datos[3].toUpperCase());
+                    double calificacion = datos[4].isBlank() ? 0 : Double.parseDouble(datos[4]);
+                    LocalDate fechaEstreno = datos[5].isBlank() ? null : LocalDate.parse(datos[5]);
+                    Idioma idioma = datos[6].isBlank() ? Idioma.INGLES : Idioma.valueOf(datos[6].toUpperCase());
+                    Calidad calidad = datos[7].isBlank() ? Calidad.BAJA : Calidad.valueOf(datos[7].toUpperCase());
+
+                    Contenido  contenido;
+
+                    if (tipo.equals(tipoContenido)) {
+                        contenido = new Pelicula(titulo, duracion, genero, idioma, calidad, calificacion);
+                    } else {
+                        contenido = new Documental(titulo, duracion, genero, idioma, calidad, calificacion, datos[8]);
+                    }
+
                     contenido.setFechaEstreno(fechaEstreno);
-
                     peliculasDesdeArchivo.add(contenido);
                 }
             });
